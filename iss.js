abe2dc9cbcc939e87a2a9898b6808fd627c01c54
EDonnellan <elaine.donny@gmail.com>
2023-02-17 04:24:28 +0000

    updated to relay the ip or error. extra custom status code handling error log made too!

--- iss.js
+++ iss.js
@@ -1,1 +1,37 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const request = require("request");
 
+
+const fetchMyIP = (callback) => {
+  const url = "https://api.ipify.org?format=json";
+
+  // use request to fetch IP address from JSON API
+  request.get(url, (error, response, body) => {
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    if (error) {
+      callback(error, null);
+      return;
+
+    } else {
+
+      const data = JSON.parse(body);
+      callback(null, data.ip);
+      return;
+    }
+  });
+};
+
+
+
+module.exports = { fetchMyIP };
